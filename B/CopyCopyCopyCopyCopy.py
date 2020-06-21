29b3199f679cb4bc0c5630de9a6002f1925c5662
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:30 +0530

    added solutions to repo

---
+++ B/CopyCopyCopyCopyCopy.py
@@ -0,0 +1,10 @@
+# https://codeforces.com/problemset/problem/1325/B
+# CopyCopyCopyCopyCopy
+
+def solve(arr):
+   return len(set(arr))
+testcases = int(input())
+for i in range(testcases):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    print(solve(arr))
