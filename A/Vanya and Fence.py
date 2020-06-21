a559548579dd682c7bb27a8d9521d851e404516f
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:35 +0530

    added solutions to repo

---
+++ A/Vanya and Fence.py
@@ -0,0 +1,14 @@
+# https://codeforces.com/problemset/problem/677/A
+# Vanya and Fence
+
+def solve(arr,w):
+    count = 0
+    for i in arr:
+        if i&gt;w:
+            count+=2
+        else:
+            count+=1
+    return count
+n,w = list(map(int,input().split()))
+arr = list(map(int,input().split()))
+print(solve(arr,w))
