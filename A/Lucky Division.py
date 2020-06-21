6d5810e90b584f7c3a05e0d58d3e48a2900a2aa4
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:25 +0530

    added solutions to repo

---
+++ A/Lucky Division.py
@@ -0,0 +1,13 @@
+# https://codeforces.com/problemset/problem/122/A
+# Lucky Division
+
+n = int(input())
+state = True
+luckies = [4,7,47,74,447,474,477,774,747,744]
+for i in range(1,n+1):
+    if n%i==0 and i in luckies:
+        print("YES")
+        state = False
+        break
+if(state):
+    print("NO")
