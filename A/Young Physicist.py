f575ed0f5e4530e64933dd48c533830e14cdf51a
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:27 +0530

    added solutions to repo

---
+++ A/Young Physicist.py
@@ -0,0 +1,14 @@
+# https://codeforces.com/problemset/problem/69/A
+# Young Physicist
+
+n = int(input())
+x,y,z = 0,0,0
+for i in range(n):
+    arr = list(map(int,input().split()))
+    x+=arr[0]
+    y+=arr[1]
+    z+=arr[2]
+if x==0 and y==0 and z==0 :
+    print("YES")
+else:
+    print("NO")
