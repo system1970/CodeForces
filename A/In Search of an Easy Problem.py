c1ca01c659ae5bc0d005a838fae6fb5c2f3cc179
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:33 +0530

    added solutions to repo

---
+++ A/In Search of an Easy Problem.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/problemset/problem/1030/A
+# In Search of an Easy Problem
+
+def solve(arr):
+    if 1 in arr:
+        return "HARD"
+    return "EASY"
+    
+n = list(map(int,input().split()))
+arr = list(map(int,input().split()))
+print(solve(arr))
