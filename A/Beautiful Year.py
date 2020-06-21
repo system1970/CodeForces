c9df84fb48a3c53dd26e4c6a8e7e8b756bca6a74
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:38 +0530

    added solutions to repo

---
+++ A/Beautiful Year.py
@@ -0,0 +1,8 @@
+# https://codeforces.com/problemset/problem/271/A
+# Beautiful Year
+
+n = list(input())
+ori = n
+while(len(set(n))!=len(n) or n==ori):
+    n = list(str(int(''.join(n))+1))
+print(''.join(n))
