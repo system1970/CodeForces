a208f4d54a8600eee11abff0c7e34d4e4e06fca7
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:24 +0530

    added solutions to repo

---
+++ A/HQ9+.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/problemset/problem/133/A
+# HQ9+
+
+string = list(input())
+state = False
+for i in string:
+    if i=='H' or i=='Q' or i=="9":
+        print("YES")
+        state = True
+        break
+if state==False:
+    print("NO")
