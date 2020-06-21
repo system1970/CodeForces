01652789dcc84d376b550f17d96a9b9c1ca45f55
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:29 +0530

    added solutions to repo

---
+++ A/Hulk.py
@@ -0,0 +1,22 @@
+# https://codeforces.com/problemset/problem/705/A
+# Hulk
+
+def solve(n):
+    ans = "I hate that "
+    for i in range(2,n):
+        if i%2==0:
+           ans+="I love that "
+        else:
+            ans+="I hate that "
+    if n%2==0:
+        ans+="I love it"
+    else:
+        ans+="I hate it"
+    return ans
+            
+           
+n = int(input())
+if n!=1:
+    print(solve(n))
+else:
+    print("I hate it")
