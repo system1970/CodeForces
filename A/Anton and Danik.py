dd0996ea3c2e13f4e0a7eb96be443990440c1820
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:37 +0530

    added solutions to repo

---
+++ A/Anton and Danik.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/problemset/problem/734/A
+# Anton and Danik
+
+def solve(games):
+    if games.count('D')&gt;games.count('A'):
+        return "Danik"
+    elif games.count('D')&lt;games.count('A'):
+        return "Anton"
+    return "Friendship"
+n = int(input())
+games = list(input())
+print(solve(games))
