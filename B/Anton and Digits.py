32a8bd81c4f25991484009e4525b238864796e95
system1970 <prabhakaran.code@gmail.com>
2020-06-21 23:30:32 +0530

    added solutions to repo

---
+++ B/Anton and Digits.py
@@ -0,0 +1,14 @@
+# https://codeforces.com/problemset/problem/734/B
+# Anton and Digits
+
+def solve(arr):
+    ans = 0
+    ans += (256 * min(arr[0],arr[2],arr[3]))
+    arr[0] -= min(arr[0],arr[2],arr[3])
+    arr[2] -= min(arr[0],arr[2],arr[3])
+    arr[3] -= min(arr[0],arr[2],arr[3])
+    ans += (32 * min(arr[0],arr[1]))
+    return ans
+    
+arr = list(map(int,input().split()))
+print(solve(arr))
