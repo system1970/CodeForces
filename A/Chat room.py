c191f269973a8ff59f1353dd83559135c462cbe5
system1970 <prabhakaran.code@gmail.com>
2020-07-19 23:30:35 +0530

    added solutions to repo

---
+++ A/Chat room.py
@@ -0,0 +1,16 @@
+# https://codeforces.com/problemset/problem/58/A
+# Chat room
+
+s = list(input())
+need = ['h','e','l','l','o']
+count=0
+for i in s:
+    if i==need[0]:
+        count+=1
+        need.pop(0)
+    if len(need)==0:
+        break
+if len(need)==0:
+    print("YES")
+else:
+    print("NO")
