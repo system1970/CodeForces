9293e801eb88d84646a1d2445130eff3cec486c7
system1970 <prabhakaran.code@gmail.com>
2020-07-28 23:30:24 +0530

    added solutions to repo

---
+++ A/Magnets.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/344/A
+# Magnets
+
+def solve():
+    n =int(input())
+    groups = 1
+    tracker = list(input())[1]
+    for j in range(n-1):
+        try:
+            i = list(input())
+            if i[0]==tracker:
+                groups+=1
+            tracker = i[1]
+        except Exception as e:
+            print(e)
+    return groups
+
+print(solve())
