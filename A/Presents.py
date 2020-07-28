863fd92fe7d64919e978afb78e403a1112ea6e85
system1970 <prabhakaran.code@gmail.com>
2020-07-28 23:30:26 +0530

    added solutions to repo

---
+++ A/Presents.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/136/A
+# Presents
+
+def solve(arr):
+    out = [0 for i in range(len(arr)+1)]
+    for i in range(len(arr)+1):
+        try:
+            out[arr[i-1]] = i
+        except:
+            print(arr[i])
+    out.pop(0)
+    print(*out)
+    
+testcases = 1
+for i in range(testcases):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    solve(arr)
