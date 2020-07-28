d5899d1e237d9cb376d775bf58d98fe38ce4e478
system1970 <prabhakaran.code@gmail.com>
2020-07-28 23:30:27 +0530

    added solutions to repo

---
+++ B/Queue at the School.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/266/B
+# Queue at the School
+
+def solve(queue,x):
+    modded = list(queue)
+    for j in range(x):
+        for i in range(len(queue)-1):
+            if list(queue)[i]=="B" and list(queue)[i+1]=="G":
+                temp = modded[i]
+                modded[i] = modded[i+1]
+                modded[i+1] = temp
+        queue = ''.join(modded)
+    print(''.join(modded))
+testcases = 1
+for i in range(testcases):
+    n,x = list(map(int,input().split()))
+    queue = input()
+    solve(queue,x)
