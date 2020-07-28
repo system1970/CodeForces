d851ba0d9a46b7b38df5fdb3736535aff2a2f479
system1970 <prabhakaran.code@gmail.com>
2020-07-28 23:30:23 +0530

    added solutions to repo

---
+++ A/Ultra-Fast Mathematician.py
@@ -0,0 +1,13 @@
+# https://codeforces.com/problemset/problem/61/A
+# Ultra-Fast Mathematician
+
+def solve():
+    a = list(input())
+    b = list(input())
+    for i in range(len(a)):
+        if a[i]!=b[i]:
+            print(1,end="")
+        else:
+            print(0,end="")
+
+solve()
