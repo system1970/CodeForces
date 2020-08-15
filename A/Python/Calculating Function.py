4f1ade72f15e87ea08e6023d9fe55820c4e64fe3
system1970 <prabhakaran.code@gmail.com>
2020-08-15 11:00:18 -0700

    added solutions to repo

---
+++ A/Python/Calculating Function.py
@@ -0,0 +1,10 @@
+# https://codeforces.com/problemset/problem/486/A
+# Calculating Function
+
+import math
+def solve(n):
+    if n%2!=0:
+        print("-",end="")
+    print(math.ceil(n/2))
+n = int(input())
+solve(n)
