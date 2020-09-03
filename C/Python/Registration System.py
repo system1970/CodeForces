e3ad7f4ecbd0909baeaf34edd9591684b40b50df
system1970 <prabhakaran.code@gmail.com>
2020-09-03 11:00:21 -0700

    added solutions to repo

---
+++ C/Python/Registration System.py
@@ -0,0 +1,16 @@
+# https://codeforces.com/problemset/problem/4/C
+# Registration System
+
+n = int(input())
+names= []
+hash_map = {}
+for i in range(n):
+    names.append(input())
+for i in names:
+    if i in hash_map:
+        print(i+str(hash_map[i]))
+        hash_map[i]+=1
+    else:
+        print("OK")
+        hash_map[i]=1
+    
