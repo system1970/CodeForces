d041b9f9f1a860e93b8ec2ba8b47058e6003a7a6
system1970 <prabhakaran.code@gmail.com>
2020-09-03 11:00:23 -0700

    added solutions to repo

---
+++ C/Python/Move Brackets.py
@@ -0,0 +1,28 @@
+# https://codeforces.com/problemset/problem/1374/C
+# Move Brackets
+
+for i in range(int(input())):
+
+    n = int(input())
+    
+    s = list(input())
+    
+    ans,bal =0,0
+    
+    for i in range(n):
+        if s[i]=="(":
+
+            bal+=1
+
+        else:
+
+            bal-=1
+
+            if bal&lt;0:
+
+                bal = 0
+
+                ans+=1
+
+    print(ans)
+        
