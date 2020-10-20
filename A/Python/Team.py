d6781f915e6095e075aaf99aac76da72571111e5
system1970 <prabhakaran.code@gmail.com>
2020-10-20 23:30:22 +0530

    added solutions to repo

---
+++ A/Python/Team.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/problemset/problem/231/A
+# Team
+
+def solve(n):
+    ans = 0
+    for i in range(n):
+        arr = list(map(int,input().split()))
+        if sum(arr)&gt;=2:
+            ans+=1
+    print(ans)
+
+solve(int(input()))
