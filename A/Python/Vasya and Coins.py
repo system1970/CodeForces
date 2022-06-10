691375c419b6a112613305e51e01b9794138a34b
system1970 <prabhakaran.code@gmail.com>
2022-06-10 23:30:20 +0530

    added solutions to repo

--- A/Python/Vasya and Coins.py
+++ A/Python/Vasya and Coins.py
@@ -1,1 +1,13 @@
-[]
+# https://codeforces.com/problemset/problem/1660/A
+# Vasya and Coins
+
+def solve():
+
+    a,b = list(map(int,input().split()))
+
+    return a + 2 * b + 1 if a &gt; 0 else 1
+
+
+for _ in range(int(input())):
+
+    print(solve())
